256a6c756b3480ce9915e9c2c9c20d4d22a5d2a6
neerajkumar790652 <92433406+neerajkumar790652@users.noreply.github.com>
2021-12-31 14:14:11 +0530

    Add files via upload

---
+++ 23Dec/kthlargest.js
@@ -0,0 +1,6 @@
+let array = [4,2,8,4,5,12,45,31,50];
+let result=array.sort(function(a,b)
+{return a-b})
+let n=result.length;
+let k =2;
+console.log(result[n-k])
---
+++ 23Dec/kthsmallest.js
@@ -0,0 +1,7 @@
+let array=[4,2,8,9,3,7,12];
+ let result=array.sort(function(a,b){
+     return a-b
+ });
+ let n=result.length;
+ let k=2;
+ console.log(result[k]);
